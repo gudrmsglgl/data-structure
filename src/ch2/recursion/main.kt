ec781126548adbdbc18f6d9eca977998346ed831
gudrmsglgl <gudrmsglgl@gmail.com>
2023-11-18 22:48:05 +0900

    # 꼬리호출 최적화(Tail Call Optimization) - factorialAdvanced

--- src/ch2/recursion/main.kt
+++ src/ch2/recursion/main.kt
@@ -1,8 +1,16 @@
 package ch2.recursion
 
 fun main() {
+    println("tailrec 6! > ${factorialAdvanced(6)}")
     println("6! > ${factorial(6)}")
 }
+
+private tailrec fun factorialAdvanced(n: Int, result: Int = 1): Int {
+    if (n == 1) return result
+
+    return factorialAdvanced(n-1, n * result)
+}
+
 
 // 5! >n: 5 >  n * 4 * 3 * 2 * 1 >> n * 4!
 // 4! >n: 4 >  n * 3 * 2 * 1 >> n * 3!
