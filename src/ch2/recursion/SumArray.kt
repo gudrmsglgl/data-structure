043d0998118c38633bb1e7583d9da447ba5e693b
gudrmsglgl <gudrmsglgl@gmail.com>
2023-11-25 23:42:16 +0900

    # 꼬리호출 최적화(Tail Call Optimization) - SumArray

--- src/ch2/recursion/SumArray.kt
+++ src/ch2/recursion/SumArray.kt
@@ -3,6 +3,7 @@ package ch2.recursion
 fun main() {
     val exArray = arrayOf(0,1,2,3,4,5)
     println("[0,1,2,3,4,5] sum >> ${sumArray(exArray)}")
+    println("[0,1,2,3,4,5] sumArrayAdvanced >> ${sumArrayAdvanced(exArray)}")
 }
 
 // [1, 2, 3, 4, 5] > [1,2,3,4] + 5
@@ -15,4 +16,10 @@ private fun sumArray(input: Array<Int>): Int {
     //val excludeLastIndexArray = input.sliceArray(IntRange(0, input.lastIndex -1))
     val excludeLastIndexArray = input.sliceArray(0 .. input.lastIndex.minus(1))
     return sumArray(excludeLastIndexArray) + input[input.lastIndex]
+}
+
+private tailrec fun sumArrayAdvanced(input: Array<Int>, result: Int = 0): Int {
+    if (input.size == 1) return result
+    val excludeLastIndexArray = input.sliceArray(0 .. input.lastIndex.minus(1))
+    return sumArrayAdvanced(excludeLastIndexArray, result + input[input.lastIndex])
 }
