004d1d8190bdb8a5ca6a7933b5edd5d0e1979ed9
gudrmsglgl <gudrmsglgl@gmail.com>
2024-01-01 15:46:55 +0900

    # Selection sort

--- src/ch2/sort/main.kt
+++ src/ch2/sort/main.kt
@@ -1,8 +1,11 @@
 package ch2.sort
 
+import kotlin.math.min
+
 fun main() {
-    val array = arrayOf(4,2,3,1)
-    bubbleSort(array)
+    val array = arrayOf(9,2,10,3)
+    //bubbleSort(array)
+    selectionSort(array)
 }
 
 private fun bubbleSort(array: Array<Int>) {
@@ -31,5 +34,25 @@ private fun bubbleSort(array: Array<Int>) {
 
     array.forEachIndexed { index, value ->
         println("index[$index] > $value")
+    }
+}
+
+private fun selectionSort(array: Array<Int>) {
+    for (i in array.indices) {
+        var minValueIndex = i // init
+
+        for (j in (i+1) until array.size) {
+            if (array[j] < array[minValueIndex]) {
+                minValueIndex = j
+            }
+        }
+
+        val temp = array[i]
+        array[i] = array[minValueIndex]
+        array[minValueIndex] = temp
+    }
+
+    array.forEachIndexed { index, i ->
+        println("selection sort > index[$index] > $i")
     }
 }
