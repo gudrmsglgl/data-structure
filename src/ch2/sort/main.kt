fe4017efbb3e2234e70caabf57e76a5e1eea8014
gudrmsglgl <gudrmsglgl@gmail.com>
2024-01-07 15:30:20 +0900

    # Insertion Sort

--- src/ch2/sort/main.kt
+++ src/ch2/sort/main.kt
@@ -5,7 +5,8 @@ import kotlin.math.min
 fun main() {
     val array = arrayOf(9,2,10,3)
     //bubbleSort(array)
-    selectionSort(array)
+    //selectionSort(array)
+    insertSort(array)
 }
 
 private fun bubbleSort(array: Array<Int>) {
@@ -55,4 +56,30 @@ private fun selectionSort(array: Array<Int>) {
     array.forEachIndexed { index, i ->
         println("selection sort > index[$index] > $i")
     }
+}
+
+// 배열의 첫 번쨰는 정렬된 영역이라고 가정한다
+// 정렬되지 않은 영역을 차례대로 정렬된 영역과 비교하며 순회한다
+// 중요: 삽입할 데이터가 정렬된 데이터보다 작으면 > +1의 인덱스에 값을 복사한다
+private fun insertSort(array: Array<Int>) {
+    for (i in 1..array.lastIndex) {
+        val insertData = array[i]
+
+        var insertIndex = 0
+        for (sortedIndex in (i-1) downTo 0) {
+            if (insertData < array[sortedIndex]) {
+                array[sortedIndex+1] = array[sortedIndex]
+            } else {
+                insertIndex = sortedIndex+1
+                break
+            }
+        }
+
+        array[insertIndex] = insertData
+    }
+
+    array.forEachIndexed { index, i ->
+        println("insertSort sort > index[$index] > $i")
+    }
+
 }
